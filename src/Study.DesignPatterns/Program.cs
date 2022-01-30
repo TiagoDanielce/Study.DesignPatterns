7323d1f5be4a5a2eda5e09b88b27292a1b09156b
Tiago Danielce <48178924+TiagoDanielce@users.noreply.github.com>
2022-01-30 18:11:51 -0300

    add Singleton Pattern

---
+++ src/Study.DesignPatterns/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Study.DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var playerOne = Singleton.GetInstance;
+            playerOne.Message("Primeiro Jogador: A bola está comigo no meio do campo");
+
+            var playerTwo = Singleton.GetInstance;
+            playerTwo.Message("Segundo Jogador: recebeu a bola");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Study.DesignPatterns/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Study.DesignPatterns
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance is null)
+                    instance = new Singleton();
+
+                return instance;
+            }
+        }
+
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
